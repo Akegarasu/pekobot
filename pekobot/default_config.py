028e19a7ef9555aa7c31cb89b1a317ebfa96bb3b
Akiba <36563862+Akegarasu@users.noreply.github.com>
2021-04-29 00:14:07 +0800

    Create default_config.py

---
+++ pekobot/default_config.py
@@ -0,0 +1,9 @@
+from datetime import timedelta
+from typing import Collection, Union, Iterable, Pattern, Optional, Dict, Any
+
+COMPRESS: int = 1
+DEBUG: bool = True
+
+API_VERSION: int = 3
+API_URL: str = f"https://www.kaiheila.cn/api/v{API_VERSION}"
+
